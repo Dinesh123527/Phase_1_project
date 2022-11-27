691dc65a0107306697e9fe8170d0b558856e3d6b
Dinesh123527 <varmadinesh249@gmail.com>
2022-11-27 06:38:57 +0530

    first commit

---
+++ src/com/lockedme/MainRunner.java
@@ -0,0 +1,11 @@
+package com.lockedme;
+
+public class MainRunner {
+    public static void main(String[] args) {
+
+        // Create "main" folder if not present in current folder structure
+        FileOperations.createMainFolderIfNotPresent("main");
+        MenuOptions.printWelcomeScreen("LockedMe", "G V Narasimha Raju");
+        HandleOptions.handleWelcomeScreenInput();
+    }
+}
